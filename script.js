e67b11bd671019188b6f63eef8bbfb63f313ad17
LyzDigital <92085964+LyzDigital@users.noreply.github.com>
2021-10-08 14:07:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -71,3 +71,15 @@ $(document).ready(function(){
         }
     });
 });
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/lyzdigital.me/Rico?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
